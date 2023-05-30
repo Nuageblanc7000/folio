e9b2cbdd4f6fc6d3985908443007203fc1a048d5
wetterene <wetterene.remy@gmail.com>
2023-05-30 17:11:32 +0200

    modif globalService

--- client/folio/src/app/shared/services/global.service.ts
+++ client/folio/src/app/shared/services/global.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalService {
-  public API_URL: string = 'http://localhost:5000/api/';
-  public API_IMG_URL: string = 'http://localhost:5000/projects/';
+  // public API_URL: string = 'http://localhost:5000/api/';
+  // public API_IMG_URL: string = 'http://localhost:5000/projects/';
+  public API_URL: string = 'http://http://rw-folio.frinki-ad.com/api/';
+  public API_IMG_URL: string = 'http://http://rw-folio.frinki-ad.com/projects/';
 }
