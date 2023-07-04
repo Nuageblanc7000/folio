bf63bef87996c79a59ab54da1594da39572b0f3f
wetterene <wetterene.remy@gmail.com>
2023-07-04 14:20:20 +0200

    mise à jour du transport sendinblue -> brevo2

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -1,5 +1,4 @@
 const sendmail = require("../config/mailer.config");
-const { ErrorResponse } = require("../utils/Responses/Error.response");
 const { SuccessResponse } = require("../utils/Responses/Succes.response");
 const messageController = {
   /**
@@ -9,11 +8,8 @@ const messageController = {
    * @param {import("express").NextFunction} next
    */
   sendMessage: async (req, res, next) => {
-    const { message, email, name } = req.body;
-    console.log(message, email, name);
-    return res
-      .status(200)
-      .json(new SuccessResponse(`${(message, email, name)}`, 200));
+    const body = req.body;
+    return res.status(200).json(new SuccessResponse(body, 200));
     // } catch (e) {
     //   console.log(e);
     //   return res.status(400).json(e);
