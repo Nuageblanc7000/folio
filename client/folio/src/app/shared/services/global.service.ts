19a3599cf589356c765b8cc4db50a996ed115023
wetterene <wetterene.remy@gmail.com>
2023-07-04 09:50:25 +0200

    mise à jour en https + link

--- client/folio/src/app/shared/services/global.service.ts
+++ client/folio/src/app/shared/services/global.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
   // public API_URL: string = 'http://localhost:5000/api/';
   // public API_IMG_URL: string = 'http://localhost:5000/projects/';
-  protected API_URL: string = 'https://rw-folio.frinki-ad.com/api/';
-  protected API_IMG_URL: string = 'https://rw-folio.frinki-ad.com/projects/';
+  public API_URL: string = 'https://rw-folio.frinki-ad.com/api/';
+  public API_IMG_URL: string = 'https://rw-folio.frinki-ad.com/projects/';
 }
