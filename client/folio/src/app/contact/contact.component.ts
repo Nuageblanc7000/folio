41406b6044b8cc4febe8130eac41e847170600e1
wetterene <wetterene.remy@gmail.com>
2023-07-04 15:18:24 +0200

    mise à jour du transport sendinblue -> brevo2

--- client/folio/src/app/contact/contact.component.ts
+++ client/folio/src/app/contact/contact.component.ts
@@ -70,13 +70,11 @@ export class ContactComponent {
 
   onSubmit() {
     if (this.formContact.valid) {
-      console.log(this.formContact.value);
       this.unsubscribe.add(
         this.contactService.sendMessage(this.formContact.value).subscribe({
           next: () => {
             this.contactService.isSending$.next(false);
           },
-          error: (err) => console.log(err),
         })
       );
     } else {
